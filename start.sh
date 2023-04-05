9db9585c8331b09013bde5c35aa459b5f86c674f
julianpajo <80450129+julianpajo@users.noreply.github.com>
2023-04-05 15:10:12 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+cd src/
 npm install phantomjs-prebuilt@2.1.16 --ignore-scripts
 npm install
-cd src/
 bower install
 cd ../
 npm install grunt
 sudo gem install compass
-npm install --save-dev coffeescript
-npm install grunt --save-dev
 cd src/
-npm install --save-dev time-grunt
+npm install --save-dev coffeescript
 grunt build-local --target=local
 cd dist/
 node server.js
